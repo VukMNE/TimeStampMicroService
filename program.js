40139ef1dfc02724ef181bbbb44d83070b28d2b3
Vuk Đuranović <vuk.djuranovic@udg.edu.me>
2017-12-26 16:40:40 +0100

    FIxed bug. Unix input allowed

--- program.js
+++ program.js
@@ -8,21 +8,23 @@ app.get("/:dateInput", function(request, response){
 	var stringTime = null;
 	if(isNaN(parseInt(request.params.dateInput))){
 		dateInput = new Date(request.params.dateInput);
-		if(dateInput==null){
+		console.log(typeof dateInput);
+		if(dateInput != null){
 			stringTime =  strftime('%B %d, %Y', dateInput);
 		}
 	}
 	else{
 		//response.send(request.params);
 		dateInput = new Date(parseInt(request.params.dateInput)*1000);
+		stringTime =  strftime('%B %d, %Y', dateInput);
 	}
 	//response.send(parseInt(request.params.dateInput));
-	console.log(parseInt(request.params.dateInput));
 	unix = dateInput.getTime()/1000;
 	var dateObject = {'unix': unix, 'natural': stringTime};
 	//response.writeHead(200, {'content-type' : 'text/json'});
 	response.send(dateObject);
 	response.end();
 });
-app.listen(process.env.PORT || 5000);
+//app.listen(process.env.PORT || 5000);
+app.listen(2702);
 
