f9ada0bcc1b2e03b5310b645ffccdaca03dbdabd
Vuk Đuranović <vuk.djuranovic@udg.edu.me>
2017-12-26 18:37:10 +0100

    Finished functional code

--- program.js
+++ program.js
@@ -8,23 +8,19 @@ app.get("/:dateInput", function(request, response){
 	var stringTime = null;
 	if(isNaN(parseInt(request.params.dateInput))){
 		dateInput = new Date(request.params.dateInput);
-		console.log(typeof dateInput);
-		if(dateInput != null){
+		if(dateInput.toDateString() != "Invalid Date"){
 			stringTime =  strftime('%B %d, %Y', dateInput);
 		}
 	}
 	else{
-		//response.send(request.params);
 		dateInput = new Date(parseInt(request.params.dateInput)*1000);
 		stringTime =  strftime('%B %d, %Y', dateInput);
 	}
-	//response.send(parseInt(request.params.dateInput));
 	unix = dateInput.getTime()/1000;
 	var dateObject = {'unix': unix, 'natural': stringTime};
-	//response.writeHead(200, {'content-type' : 'text/json'});
+
 	response.send(dateObject);
 	response.end();
 });
 app.listen(process.env.PORT || 5000);
-//app.listen(2702);
 
