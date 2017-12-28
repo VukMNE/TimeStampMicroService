8c6fb3ef0aadeae0a543894e8ae39dd5599c41e7
Vuk Đuranović <vuk.djuranovic@udg.edu.me>
2017-12-28 19:24:03 +0100

    CORS fix

--- program.js
+++ program.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var strftime = require("strftime");
+var cors = require("cors");
 var app = express();
+app.use(cors());
 app.use(express.static('./public'));
 app.get("/:dateInput", function(request, response){
 	var dateInput;
